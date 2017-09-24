879516479067ccf0c00073dc974c7572ab06e8b0
Nate Vaughan <nvaughanguitar@yahoo.com>
2017-09-24 13:59:38 -0500

    Second fold example to dedup a list of longs

--- src/main/kotlin/com/natevaughan/koach/sandbox/FoldExample.kt
+++ src/main/kotlin/com/natevaughan/koach/sandbox/FoldExample.kt
@@ -5,8 +5,14 @@ package com.natevaughan.koach.sandbox
  */
 fun sumAndCount(acc: SumAndCount, value: Double): SumAndCount {
     return SumAndCount(acc.sum + value, acc.count + 1);
-
 }
 
 
 data class SumAndCount(val sum: Double, val count: Long)
+
+fun deDup(acc: Array<Long>, next: Long): Array<Long> {
+    if (acc.contains(next)) {
+        return acc
+    }
+    return acc + next
+}
--- src/test/kotlin/com/natevaughan/koach/FoldTest.kt
+++ src/test/kotlin/com/natevaughan/koach/FoldTest.kt
@@ -1,6 +1,7 @@
 package com.natevaughan.koach
 
 import com.natevaughan.koach.sandbox.SumAndCount
+import com.natevaughan.koach.sandbox.deDup
 import com.natevaughan.koach.sandbox.sumAndCount
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -11,10 +12,17 @@ import org.junit.Test
 class FoldTest {
 
     @Test
-    fun testKotlinFold() {
+    fun testSumAndCountFold() {
         val values = listOf(1.0, 2.0, 3.0, 4.0, 5.0)
         val result: SumAndCount = values.fold(SumAndCount(0.0, 0), ::sumAndCount)
         assertTrue(result.sum == 15.0)
         assertTrue(result.count == 5L)
+    }
+
+    @Test
+    fun testDedupFold() {
+        val values = arrayOf(1L, 2L, 2L, 5L, 1L)
+        val result: Array<Long> = values.fold(arrayOf(values.first()), ::deDup)
+        assertTrue(result.size == 3)
     }
 }
